55573352a1b005ce53ea2342915ca370881573dd
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-01-17 09:17:17 +1100

    Split collapse_nodes() into it's two constituent parts

--- src/manipulation.hpp
+++ src/manipulation.hpp
@@ -38,17 +38,29 @@ namespace forest {
 // TODO: This would be nice if it worked on subtrees too (just need to
 // class it appropriately, etc).  Then we could apply different rules
 // to different parts of the tree.
+//
+// NOTE: collapse_node does not check that a node is actually
+// collapsable -- it just does it.
+//
+// NOTE: I am not 100% sure that the assignment here causes a polite
+// cleanup: erase seems more what I'd want to do, but it cleans up too
+// much.  This should do the whole subtree assignment thing so I guess
+// the Right Thing is being done.
+template <typename T>
+void collapse_node(typename treetree::tree<T>::sub_post_iterator singleton) {
+  typedef typename treetree::tree<T>::sub_post_iterator sub_post_iterator;
+  sub_post_iterator descendant = boost::prior(singleton);
+  descendant->begin()->length_ += singleton->begin()->length_;
+  *singleton = *descendant;
+}
+
 template <typename T>
 void collapse_singles(treetree::tree<T>& tr) {
   typedef typename treetree::tree<T>::sub_post_iterator sub_post_iterator;
   for (sub_post_iterator it = tr.begin_sub_post();
-       it != tr.end_sub_post(); ++it) {
-    if (it->arity() == 1) {
-      sub_post_iterator singleton = it, descendant = boost::prior(it);
-      descendant->begin()->length_ += singleton->begin()->length_;
-      *singleton = *descendant;
-    }
-  }
+       it != tr.end_sub_post(); ++it)
+    if (it->arity() == 1)
+      collapse_node<T>(it);
 }
 
 }
