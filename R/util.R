05d657f020e0021c474fb80168f8df7c5d7ecae8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-04-18 19:22:23 +1000

    More assertions.

--- R/util.R
+++ R/util.R
@@ -3,3 +3,21 @@ check_scalar <- function(x) {
     stop(sprintf("%s must be a scalar", deparse(substitute(x))))
   }
 }
+
+assert_unit <- function(x) {
+  if (!is.unit(x)) {
+    stop(sprintf("%s must be a unit", deparse(substitute(x))))
+  }
+}
+
+assert_number <- function(x) {
+  if (!(is.numeric(x) || is.integer(x))) {
+    stop(sprintf("%s must be numeric", deparse(substitute(x))))
+  }
+}
+
+assert_grob <- function(x) {
+  if (!is.grob(x)) {
+    stop(sprintf("%s must be a grob", deparse(substitute(x))))
+  }
+}
